7aa553a3189c0c85b16687612f44504a043f5505
yakovlev.nv <115099088+yakovlev-nv@users.noreply.github.com>
2022-12-13 21:40:55 +0300

    Update main.py

--- Лабораторная 1/main.py
+++ Лабораторная 1/main.py
@@ -2,19 +2,20 @@
 import doctest
 from typing import Union
 
+
 class MoneyBank:
     """ Класс "копилка" c двумя атрибутами: вместимость и кол-во монет внутри
     >>> bank = MoneyBank(120,0)
-
     """
+    
     def __init__(self, capacity:int, coins_inside:int):
+        # capacity - вместимость копилки, coins_inside - кол-во монет внутри
         if not  isinstance(capacity,int):
             raise TypeError
         if not isinstance(coins_inside,int):
             raise TypeError
         self.capacity = capacity
         self.coins_inside = coins_inside
-
 
     def add(self, coins:int):
         # Добавить монет в копилку, если достаточно места
@@ -23,12 +24,10 @@ class MoneyBank:
         if not (self.capacity - self.coins_inside) >= coins:
             raise ValueError
         else:
-            self.capacity -= coins
             self.coins_inside += coins
 
-
     def get(self, coins:int):
-    # Достать монеты из копилки, если внутри есть такое количество
+        # Достать указанное количество монет из копилки, если внутри есть такое количество
         if not isinstance(coins, int):
             raise TypeError
         if not self.coins_inside > coins:
@@ -36,18 +35,20 @@ class MoneyBank:
         else:
             self.coins_inside -= self.coins
 
+
 class ElectricKettle:
     """ Класс электрический чайник с тремя атрибутамии - емкость, включен или нет и объем воды внутри
     >>> kettle = ElectricKettle(2, 0.5)
-
     """
-    def __init__(self, max_volume: Union[int, float],water_inside: Union[int, float],):
+    
+    def __init__(self, max_volume: Union[int, float],water_inside: Union[int, float]):
         self.max_volume = max_volume
         self.water_inside = water_inside
         if not isinstance(max_volume, (int, float)):
             raise TypeError
         if not isinstance(water_inside, (int, float)):
             raise TypeError
+            
     def add_water(self, water:Union[int, float]):
         # налить воды в чайник
         if not isinstance(water, (int, float)):
@@ -56,6 +57,7 @@ class ElectricKettle:
             raise ValueError
         else:
             self.water_inside += water
+            
     def pour_water(self, water:Union[int, float]):
         # вылить воду из чайника
         if not isinstance(water, (int, float)):
@@ -64,14 +66,13 @@ class ElectricKettle:
             raise ValueError
         else:
             self.water_inside -= water
-
-
-
+            
+            
 class TV:
     """Класс телевизор с двумя аттрибутами - длина диагонали разрешение экрана
     >>> tv = TV(55,'FullHD')
-
     """
+    
     def __init__(self, diagonal:Union[int, float], resolution:str):
         self.diagonal = diagonal
         self.resolution = resolution
@@ -79,13 +80,16 @@ class TV:
             raise TypeError
         if not isinstance(resolution, str):
             raise TypeError
+            
     def turn_on(self):
         # включить телевизор
         ...
+        
     def switch_channel(self):
         # проверка если теелвизор включен, то переключить канал
         ...
+        
+        
 if __name__ == "__main__":
     # TODO работоспособность экземпляров класса проверить с помощью doctest
-    pass
-doctest.testmod()
+    doctest.testmod()
