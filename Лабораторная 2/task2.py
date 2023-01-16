95a0c24dcd65593c59493975a20e755d3b111ceb
yakovlev.nv <115099088+yakovlev-nv@users.noreply.github.com>
2023-01-17 02:36:42 +0300

    Update task2.py

--- Лабораторная 2/task2.py
+++ Лабораторная 2/task2.py
@@ -1,13 +1,18 @@
 from task1 import Book, BOOKS_DATABASE
 
 class Library:
-    def __init__(self, books = []):
+    def __init__(self, books = None):
         self.books = books
 
     def get_next_book_id(self):
-        return len(self.books)+1
+    """ возвращает идентификатор для добавления новой книги в библиотеку """
+        if self.books:
+            return self.books[-1].id_
+        else:
+            return 1
 
     def get_index_by_book_id(self, id_):
+    """ возвращает индекс книги в списке """
         for index, book in enumerate(self.books):
             if book.id_ == id_:
                 return self.books.index(book)
