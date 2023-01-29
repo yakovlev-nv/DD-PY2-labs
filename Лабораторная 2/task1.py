3ddbe596ac2fb2a53db44f9fdc8a40751cc62938
yakovlev.nv <115099088+yakovlev-nv@users.noreply.github.com>
2023-01-29 03:04:25 +0300

    Update task1.py

--- Лабораторная 2/task1.py
+++ Лабораторная 2/task1.py
@@ -21,12 +21,12 @@ class Book:
         return f'Книга {self.name!r}'
 
     def __repr__(self) -> str:
-        return f"Book(id_={self.id_}, name={self.name!r}, pages={self.pages})"
+        return f"Book(id_={self.id!r}, name={self.name!r}, pages={self.pages!r})"
 
 if __name__ == '__main__':
     # инициализируем список книг
     list_books = [
-        f"Book(id_={self.id!r}, name={self.name!r}, pages={self.pages!r})" for book_dict in BOOKS_DATABASE
+        f"Book(id_={self.id}, name={self.name}, pages={self.pages})" for book_dict in BOOKS_DATABASE
     ]
     for book in list_books:
         print(book)  # проверяем метод __str__
